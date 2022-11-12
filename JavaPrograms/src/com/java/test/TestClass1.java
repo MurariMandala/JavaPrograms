a59a62f2ef5f3a1dbaaebb5b851d0936603a9507
Murari <mandalamurari@gmail.com>
2022-11-12 15:59:12 +0530

    corejava interview QA

---
+++ JavaPrograms/src/com/java/test/TestClass1.java
@@ -0,0 +1,18 @@
+package com.java.test;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class TestClass1 {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
